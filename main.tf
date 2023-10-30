f66ad8048bf85fb319c9aa6761d5e15bb00e19f9
Tishaun Bristol <Tishaunbristol@me.com>
2023-10-29 21:56:44 -0400

    terraform testing

--- main.tf
+++ main.tf
@@ -1,30 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0.0"
-    }
-  }
-  backend "s3" {
-    bucket = "api-gateway-lambda-dynamodb" # change to name of your bucket
-    region = "us-west-1"                   # change to your region
-    key    = "terraform.tfstate"
-  }
-}
-
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-2"
 }
 
-resource "aws_dynamodb_table" "questions" {
-  name           = "questions"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "id"
+resource "aws_instance" "example" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
 
-  attribute {
-    name = "id"
-    type = "S"
+  tags = {
+    Name = "var.ec2_name"
   }
 }
